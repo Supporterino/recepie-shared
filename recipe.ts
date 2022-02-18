6647faed6e9c5ba9faa6cf13a0dc4a4882c63a44
Supporterino <lars@roth-kl.de>
2022-02-18 16:01:25 +0100

    Add initial types

---
+++ recipe.ts
@@ -0,0 +1,5 @@
+import { Ingredient } from "./ingredient";
+
+export class Recipe {
+	constructor(public _id: string, public name: string, public description: string, public ingredients: Ingredient[], public steps: string[], public rating: number, public tags: string[], public owner: string, public creationTimestamp: Date, public updateTimestamp: Date) {}
+}
