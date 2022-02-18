babebdb2a4fc64f4198a3111a59675765a8941bf
Supporterino <lars@roth-kl.de>
2022-02-18 20:50:51 +0100

    Swap to interfaces and type def

--- recipe.ts
+++
@@ -1,5 +0,0 @@
-import { Ingredient } from "./ingredient";
-
-export class Recipe {
-	constructor(public _id: string, public name: string, public description: string, public ingredients: Ingredient[], public steps: string[], public tags: string[], public owner: string, public creationTimestamp: Date, public updateTimestamp: Date, public rating?: number) {}
-}
