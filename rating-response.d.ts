7e21e7962e6a33a2cdfd49d5894bf83d29a6fa73
Supporterino <lars@roth-kl.de>
2022-03-23 19:28:59 +0100

    add rating response

---
+++ rating-response.d.ts
@@ -0,0 +1,13 @@
+import { Response } from "./response";
+
+export interface RatingResponse extends Response {
+    success: boolean;
+	method: RatingOperations;
+	recipeID: string;
+	userID: string;
+}
+
+export enum RatingOperations {
+	ADD,
+	UPDATE
+}
