6ae1c03711fc6c88ecaeada3b532059efcfd2258
Supporterino <lars@roth-kl.de>
2022-05-03 19:22:56 +0200

    Revert "intermediate units"
    This reverts commit 542cd4948383df0d2afe4173870f787b4457777e.

--- enums/units.ts
+++ enums/units.ts
@@ -1,34 +1,6 @@
-export enum MetricWeights {
-	GRAM = "g",
-	KILOGRAM = "kg"
+export enum Units {
+    Grams = "g",
+    Kilograms = "kg",
+    Mililitres = "ml",
+    Litres = "l",
 }
-
-export enum ImperialWeights {
-	DRAM = "dr",
-	OUNCE = "oz",
-	POUND = "lb"
-}
-
-export enum MetricVolume {
-	LITRE = "l",
-	DECILITRE = "dl",
-	MILLILITRE = "ml"
-}
-
-export enum ImperialVolume {
-	FLUID_OUNCE = "fl oz",
-	PINT = "pt",
-	QUART = "qt",
-	GALLON = "gal",
-}
-
-export enum MetricTemperature {
-	CELSIUS = "°C"
-}
-
-export enum ImperialTemperature {
-	FAHRENHEIT = "°F"
-}
-
-export const Units = { ...MetricWeights, ...MetricTemperature, ...MetricVolume, ...ImperialWeights, ...ImperialTemperature, ...ImperialVolume };
-export type Units = MetricWeights | MetricTemperature | MetricVolume | ImperialWeights | ImperialTemperature | ImperialVolume;
