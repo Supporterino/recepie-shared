8bdf83f0187e9054d7bc15a163c70aaa52afa99b
Supporterino <lars@roth-kl.de>
2023-03-01 17:35:55 +0100

    feat: :sparkles: Add Types for PhotoUpload

--- enums/index.ts
+++ enums/index.ts
@@ -2,3 +2,4 @@ export * from "./units";
 export * from "./rating-methods";
 export * from "./roles";
 export * from "./unit_definitions";
+export * from "./photoTypes";
---
+++ enums/photoTypes.ts
@@ -0,0 +1,5 @@
+export enum PhotoTypes {
+  AVATAR = "user.profilePicture",
+  RECIPE = "recipe.primaryPicture",
+  ADDITION_RECIPE = "recipe.AdditionalPicture",
+}
